3a5d708479f0f3c9ed378272fbffc490dc8ffbce
Nikitas Filosofof <nikitas4717@gmail.com>
2021-01-25 00:25:56 +0100

    symbol indicator works

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const Gameboard = (() => {
             e.target.innerHTML = totem
             if (totem === `X`) {totem = `O`} else {totem = `X`}
             Gameboard.gameCheck()
+            Game.switchIndicator()
+
         }))
 
     const clear = () => {Gameboard.grid.forEach(element => element.innerHTML = ``)}
@@ -34,42 +36,44 @@ const Gameboard = (() => {
     return {grid, clear, gameCheck}
 })()
 
-const Human = (name, score, nameClass, scoreClass) => {
+const Human = (name, score, daddyClass, nameClass, scoreClass, sym) => {
     const getName = () => name
     const getScore = () => score
     document.querySelector(nameClass).innerHTML = name
     const visualScore = document.querySelector(scoreClass)
+
+    const parentEl = document.querySelector(daddyClass)
     const win = () => {
         score += 1
         visualScore.innerHTML = score
         Gameboard.clear()
     }
 
+    const symbolIndicat = document.querySelector(sym)
+
+    const indicate = () => {parentEl.querySelector(`.roundthingy`).classList.toggle(`bluecolor`)}
+
     const resetScore = () => {score = 0}
 
-    return {getName, getScore, win, resetScore}
+    return {getName, getScore, win, resetScore, indicate, symbolIndicat}
 }
 
 const Game = (() => {
     const players = []
     const createPlayers = () => {
-        players[0] = Human(playerOneField.value, 0, `.xPlayerName`, `.xPlayerScore`)
-        players[1] = Human(playerTwoField.value, 0, `.oPlayerName`, `.oPlayerScore`)
+        players[0] = Human(playerOneField.value, 0, `.Xplayer`,`.xPlayerName`, `.xPlayerScore`, `.Xindc`)
+        players[1] = Human(playerTwoField.value, 0,`.Oplayer` ,`.oPlayerName`, `.oPlayerScore`, `.Oindc`)
     }
+    const switchIndicator = () => {players.forEach(player => player.indicate())}
 
-
-
-    return {players, createPlayers}
+    return {switchIndicator,players, createPlayers}
 })()
-
-//let mothafucker = Human(`Motherfucker`, 0)
 
 const winLookUp = [
     [0,1,2],[3,4,5],[6,7,8],
     [0,3,6],[1,4,7,],[2,5,8],
     [0,4,8],[2,4,6]
 ]
-
 
 //EVENT LISTENERS
 
@@ -93,37 +97,4 @@ function getIndexesOF(arr, value) {
     }, [])
 }
 
-// function winCheck(symbol){
-//     const checker = (arr, target) => target.every(v => arr.includes(v));
-
-//     winLookUp.forEach(arr => { 
-//         if (checker(getIndexesOF(grid, symbol), arr)) {
-//             return true
-//         }
-//     })
-
-    
-// }
-
-
-
 window.onload = toggleForm()
-
-
-// function renderGrid() {
-//     let elements = Gameboard.grid
-//     ticker = 0
-//     elements.forEach(element => {
-//         if (ticker === 0) {
-//             element.innerHTML = `X`
-//             ticker++
-//         } else {
-//             element.innerHTML = `O`
-//             ticker--
-//         }
-//     })
-
-// }
-
-// console.log(e.target.id)
-// console.log(e.target)
